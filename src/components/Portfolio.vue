a01ded351a8b28ea5b6ae999c2a1dcc26acdb496
Zach Springer <zspringer.springer@gmail.com>
2017-10-10 14:22:28 -0600

    Added an extra row for the porfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -37,7 +37,7 @@
                   <img src="http://res.cloudinary.com/dmyvekzok/image/upload/c_scale,w_300/v1507606013/Screen_Shot_2017-10-09_at_9.26.17_PM_ft8vks.png"></img>
                 </v-card-text>
                 <v-card-title primary-title class="layout justify-center">
-                  <div class="headline">NFL</div>
+                  <a class="headline" href="https://zspringer.github.io/NFL_Roster">NFL</a>
                 </v-card-title>
                 <v-card-text>
                   Example of a project that utilizes the NFL API to create a list of players for a fantasy football team.  This project
@@ -48,18 +48,69 @@
             <v-flex xs12 md4>
               <v-card class="elevation-0 transparent">
                 <v-card-text class="text-xs-center">
-                  <v-icon x-large class="black--text text--lighten-2">build</v-icon>
+                  <!-- <v-icon x-large class="black--text text--lighten-2">build</v-icon> -->
+                  <img src="http://res.cloudinary.com/dmyvekzok/image/upload/c_scale,h_202,w_300/v1507664722/Screen_Shot_2017-10-10_at_1.42.30_PM_akwlzt.png"></img>
                 </v-card-text>
                 <v-card-title primary-title class="layout justify-center">
-                  <div class="headline text-xs-center">Completely Open Sourced</div>
+                  <a class="headline text-xs-center" href="https://zdkanban.herokuapp.com" >Kanban</a>
                 </v-card-title>
                 <v-card-text>
-                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. Pellentesque habitant morbi tristique senectus
-                  et netus et malesuada fames ac turpis egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum
-                  tincidunt. Suspendisse potenti.
+                  Example of a collaborative team full stack applicationn using Vue, Node, Bootstrap and MongoDB to build Kanban boards to better help
+                  users with project management.  This project is very similar to Trello.
                 </v-card-text>
               </v-card>
             </v-flex>
+
+            <v-flex xs12 md4>
+              <v-card class="elevation-0 transparent">
+                  <v-card-text class="text-xs-center">
+                    <!-- <v-icon x-large class="black--text text--lighten-2">color_lens</v-icon> -->
+                    <!-- <img src="http://res.cloudinary.com/dmyvekzok/image/upload/c_scale,w_300/v1502836114/Screen_Shot_2017-08-15_at_4.25.54_PM_ygl0us.png"></img> -->
+                    <img src="http://res.cloudinary.com/dmyvekzok/image/upload/c_scale,h_202,w_300/v1502836114/Screen_Shot_2017-08-15_at_4.25.54_PM_ygl0us.png"></img>
+                  </v-card-text>
+                  <v-card-title primary-title class="layout justify-center">
+                    <div class="headline text-xs-center">Inspire</div>
+                  </v-card-title>
+                  <v-card-text>
+                    Example of a project that accesses several API's called through several services and controllers to provide a daily landing page.
+                    Data is persistent through a Node server deployed to Heroku. 
+                  </v-card-text>
+                </v-card>
+            </v-flex>
+            <v-flex xs12 md4>
+              <v-card class="elevation-0 transparent">
+                <v-card-text class="text-xs-center">
+                  <!-- <v-icon x-large class="black--text text--lighten-2">flash_on</v-icon> -->
+                  <img src="http://res.cloudinary.com/dmyvekzok/image/upload/c_scale,w_300/v1507606013/Screen_Shot_2017-10-09_at_9.26.17_PM_ft8vks.png"></img>
+                </v-card-text>
+                <v-card-title primary-title class="layout justify-center">
+                  <a class="headline" href="https://zspringer.github.io/NFL_Roster">NFL</a>
+                </v-card-title>
+                <v-card-text>
+                  Example of a project that utilizes the NFL API to create a list of players for a fantasy football team.  This project
+                  was built with vanilla Javascript and Bootstrap.
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 md4>
+              <v-card class="elevation-0 transparent">
+                <v-card-text class="text-xs-center">
+                  <!-- <v-icon x-large class="black--text text--lighten-2">build</v-icon> -->
+                  <img src="http://res.cloudinary.com/dmyvekzok/image/upload/c_scale,h_202,w_300/v1507664722/Screen_Shot_2017-10-10_at_1.42.30_PM_akwlzt.png"></img>
+                </v-card-text>
+                <v-card-title primary-title class="layout justify-center">
+                  <a class="headline text-xs-center" href="https://zdkanban.herokuapp.com" >Kanban</a>
+                </v-card-title>
+                <v-card-text>
+                  Example of a collaborative team full stack applicationn using Vue, Node, Bootstrap and MongoDB to build Kanban boards to better help
+                  users with project management.  This project is very similar to Trello.
+                </v-card-text>
+              </v-card>
+            </v-flex>
+
+
+
+
           </v-layout>
         </v-container>
       </v-flex>
